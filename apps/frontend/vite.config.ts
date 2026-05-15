aadd55a2bb909903bae85b2a0d5e393cd15de5a0
minhajul islam <nishatislam3108@gmail.com>
2026-05-15 23:09:42 +0600

    update vite conf so that ff browser console does not show sse err msg

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,19 +1,17 @@
 import tailwindcss from "@tailwindcss/vite";
 import { devtools } from "@tanstack/devtools-vite";
-
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
-
 import viteReact from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-const config = defineConfig({
+export default defineConfig({
 	resolve: { tsconfigPaths: true },
 	plugins: [
-		devtools(),
+		devtools({
+			consolePiping: { enabled: false },
+		}),
 		tailwindcss(),
 		tanstackRouter({ target: "react", autoCodeSplitting: true }),
 		viteReact(),
 	],
 });
-
-export default config;
