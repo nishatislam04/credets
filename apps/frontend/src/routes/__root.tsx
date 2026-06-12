41d9dae28e7910b5f72e8f35186c91f55998c036
minhajul islam <nishatislam3108@gmail.com>
2026-06-12 06:27:34 +0600

    made some changes for tanstack dev panels. but i donno what i did

--- apps/frontend/src/routes/__root.tsx
+++ apps/frontend/src/routes/__root.tsx
@@ -1,13 +1,28 @@
+import { createReactPlugin } from "@tanstack/devtools-utils/react";
 import { TanStackDevtools } from "@tanstack/react-devtools";
-import { FormDevtoolsPanel } from "@tanstack/react-form-devtools";
+import { FormDevtoolsPanel, formDevtoolsPlugin } from "@tanstack/react-form-devtools";
 import { createRootRoute, Outlet } from "@tanstack/react-router";
-import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
+import {
+	TanStackRouterDevtools,
+	TanStackRouterDevtoolsPanel,
+} from "@tanstack/react-router-devtools";
 import { GooeyToaster } from "@/components/ui/goey-toaster";
 
 import "../styles.css";
 
 export const Route = createRootRoute({
 	component: RootComponent,
+});
+
+// Create the plugins with the utility
+const [RouterPlugin] = createReactPlugin({
+	name: "TanStack Router",
+	Component: TanStackRouterDevtoolsPanel,
+});
+
+const [FormPlugin] = createReactPlugin({
+	name: "TanStack Form",
+	Component: FormDevtoolsPanel,
 });
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -29,22 +44,8 @@ function RootComponent() {
 				<Outlet />
 			</QueryClientProvider>
 			<GooeyToaster />
-			<TanStackDevtools
-				config={{
-					theme: "light",
-					position: "bottom-right",
-				}}
-				plugins={[
-					{
-						name: "TanStack Router",
-						render: <TanStackRouterDevtoolsPanel />,
-					},
-					{
-						name: "Tanstack Form",
-						render: <FormDevtoolsPanel />,
-					},
-				]}
-			/>
+			<TanStackRouterDevtools position="bottom-right" />
+			{/*<TanStackDevtools plugins={[formDevtoolsPlugin()]} />*/}
 		</ThemeProvider>
 	);
 }
